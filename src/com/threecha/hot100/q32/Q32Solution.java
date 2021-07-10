1dea336b0a52d1ad61ba02a0bd80b418d5c274a8
threecha <threecha@163.com>
2021-07-10 14:41:40 +0800

    q32动态规划 解法存在问题 待更新

--- src/com/threecha/hot100/q32/Q32Solution.java
+++ src/com/threecha/hot100/q32/Q32Solution.java
@@ -11,6 +11,10 @@ import java.util.Stack;
  * 用当前右括号的index 减去栈顶元素的index 得到当前最大的长度
  * <p>
  * 需要在栈底 设置元素的初始化值 ")"
+ * <p>
+ * <p>
+ * <p>
+ * 思路2： 使用动态规划 解决括号匹配问题
  */
 public class Q32Solution {
     public static void main(String[] args) {
@@ -42,4 +46,43 @@ public class Q32Solution {
         }
         return result;
     }
+
+
+    /***
+     *  动态规划思路 dp[i] 是以i结尾的有效子串的长度  notice 子串必须以i结尾。
+     *  i依次向右移动 最终获取最大值即可。
+     *  1. i>2 s[i] == ')'
+     *
+     * @param s
+     * @return
+     */
+    public static int solution2(String s) {
+        int length = s.length();
+        int[] dp = new int[length];
+        int result = 0;// 记录最大的有效长度
+        // dp[i] 是以i结尾的子串的 最大有效字符串长度
+        dp[0] = 0;
+        if (s.charAt(1) == ')' && s.charAt(0) == '(') {
+            dp[1] = 2;
+        } else {
+            // 其他情况下的括号均不能构成闭环 故dp[2] =0;
+            dp[2] = 0;
+        }
+        for(int i =0;i<length;i++){
+            if(i>1){
+                if(s.charAt(i)==')'){
+                    // 若i-i 可以匹配成功那么
+                    if(s.charAt(i-1)=='('){
+                        dp[i]=dp[i-2]+2;
+                    }
+                }else{
+                    dp[i]=0;
+                }
+
+            }
+        }
+
+        return 0;
+    }
+
 }
